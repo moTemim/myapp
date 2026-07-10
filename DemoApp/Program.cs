0e4a8e68d0b1a1542ae1aa805a43929d214aee8b
Mohamed Temim Lemdaoui <temim.lemdaoui@outlook.com>
2026-07-11 00:24:42 +0100

    initial commit

---
+++ DemoApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace CoreApplication
+{
+    using System;
+    using System.Threading.Tasks;
+    public class Program
+    {
+        
+        private int total;
+
+                public int AddNumbers(int first, int second)
+        {
+            this.total = first + second;
+            return this.total;
+        }
+    }
+}
